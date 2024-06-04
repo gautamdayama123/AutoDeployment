c276166bcd09a5201528a40b2576215b3d0d16b3
gautam dayama <gautam.dayama@bt.com>
2024-06-04 14:43:29 +0530

    ui added

--- src/CRD_select/name-space/name-space.component.ts
+++ src/CRD_select/name-space/name-space.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import Namespace from '../models/Namespace';
+
 
 @Component({
   selector: 'app-name-space',
@@ -13,17 +13,5 @@ import Namespace from '../models/Namespace';
 })
 export class NameSpaceComponent {
 
-  namespace:Namespace=new Namespace({labels:{}, name:''});
-
-    addLabel(){
-      this.namespace.addLabel
-    }
-
-    // removeLabel(key:String){
-    //   delete this.namespace.metadata.labels[key];
-    // }
-
-    onSubmit(){
-      console.log('NameSpace',this.namespace)
-    }
+ 
 }
